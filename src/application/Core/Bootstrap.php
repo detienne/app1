7b0ac70990378d3040b3c504fae36009f8033597
detienne <davidetienne.ipformation@gmail.com>
2014-01-08 09:58:06 +0100

    Commit base controllers

---
+++ src/application/Core/Bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Bootstrap du module Core
+ * 
+ * @category	App1
+ * @package		Core
+ * @desc		Bootstrap du module Core
+ * @author		detienne <davidetienne.ipformation@gmail.com>
+ * 
+ */
+class Bootstrap extends Zend_Application_Module_Bootstrap
+{
+	
+}
---
+++ src/application/Core/controllers/ErrorController.php
@@ -0,0 +1,18 @@
+<?php 
+
+/**
+ * 
+ * Controller des erreurs de l'application
+ * 
+ * @category		App1
+ * @package			Core
+ * 
+ */
+
+class ErrorController extends Zend_Controller_Action
+{
+	function errorAction()
+	{
+		
+	}
+}
